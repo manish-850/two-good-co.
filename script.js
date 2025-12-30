a054c0cf72e13b35194a4b2e3b66257042245c8d
Manish Kumar <manishkumar9576700@gmail.com>
2025-12-30 11:59:11 +0530

    on page 2 hover effect added

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+function startAnim(top,bottom){
+
+    return document.querySelector(top).addEventListener("mouseenter", () => {
+            gsap.to(bottom, {
+                top: "calc(50% - 2vw)",
+                height: "22vw",
+                paddingTop: "6vw",
+                paddingBottom: "1vw",
+                ease: "expoScale",
+                duration: 0.25,
+                delay: 0.1
+            })
+        });
+}
+
+function endAnim(top,bottom){
+
+    return document.querySelector(top).addEventListener("mouseleave", () => {
+            gsap.to(bottom, {
+                top: "calc(50% - 2vw)",
+                height: "4vw",
+                paddingTop: 0,
+                paddingBottom: 0,
+                ease: "pexpoScale",
+                duration: 0.25,
+                delay: 0.1
+            })
+        });
+}
+
+startAnim("#top1", "#bottom1");
+endAnim("#top1", "#bottom1");
+
+startAnim("#top2", "#bottom2");
+endAnim("#top2", "#bottom2");
+
+startAnim("#top3", "#bottom3");
+endAnim("#top3", "#bottom3");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Initialize Lenis
 const lenis = new Lenis({
     autoRaf: true,
